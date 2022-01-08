3bb2b533b64f8d183ca51cc73d1bb75e03d6fec0
otinomz <82697198+otinomz@users.noreply.github.com>
2022-01-08 19:46:16 +0100

    new chnages

--- src/App.js
+++ src/App.js
@@ -8,17 +8,19 @@ export default function App() {
   const [result, setResult] = useState("");
 
   return (
-    <form onSubmit={handleSubmit((data) => setResult(JSON.stringify(data)))}>
-      {/* <Header /> */}
-      <input {...register("firstName")} placeholder="First name" />
-      <input {...register("lastName")} placeholder="Last name" />
-      <select {...register("category")}>
-        <option value="">Select...</option>
-        <option value="A">Category A</option>
-        <option value="B">Category B</option>
-      </select>
-      <p>{result}</p>
-      <input type="submit" />
-    </form>
+    <>
+      <h1>React Forms</h1>
+      <form onSubmit={handleSubmit((data) => setResult(JSON.stringify(data)))}>
+        <input {...register("firstName")} placeholder="First name" />
+        <input {...register("lastName")} placeholder="Last name" />
+        <select {...register("category")}>
+          <option value="">Select...</option>
+          <option value="A">Category A</option>
+          <option value="B">Category B</option>
+        </select>
+        <p>{result}</p>
+        <input type="submit" />
+      </form>
+    </>
   );
 }
